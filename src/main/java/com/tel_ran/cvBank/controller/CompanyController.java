07584515ad5513819be41e4f12462ce7c961abc4
kir.khi <garry4ever@gmail.com>
2018-10-05 12:42:53 +0300

    added hello

--- src/main/java/com/tel_ran/cvBank/controller/CompanyController.java
+++ src/main/java/com/tel_ran/cvBank/controller/CompanyController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,6 +41,11 @@ import com.tel_ran.cvBank.dto.ExperienceDto;
 
 @RestController
 public class CompanyController implements CompanyRequestType {
+	
+	@GetMapping
+	public String hello() {
+		return "Hello";
+	}
 
 	@RequestMapping(value = REGISTRATION, method = RequestMethod.POST)
 	public CompanyRegistrationDto registration(@RequestBody CompanyProfileDto company) {
