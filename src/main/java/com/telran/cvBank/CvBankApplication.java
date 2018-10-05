c12365cfd1529776ddcbbd4b2dd084b7531d9a17
kir.khi <garry4ever@gmail.com>
2018-10-05 12:57:28 +0300

    from scratch

--- src/main/java/com/telran/cvBank/CvBankApplication.java
+++ src/main/java/com/telran/cvBank/CvBankApplication.java
@@ -1,4 +1,4 @@
-package com.tel_ran.cvBank;
+package com.telran.cvBank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/telran/cvBank/controller/CVBankController.java
+++ src/main/java/com/telran/cvBank/controller/CVBankController.java
@@ -1,4 +1,4 @@
-package com.tel_ran.cvBank.controller;
+package com.telran.cvBank.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
-public class CompanyController {
+public class CVBankController {
 	
 	@GetMapping
 	public String hello() {
